3199ede07154d11119880997b46f8fe0248b1cd9
munyite001 <emunyite@gmail.com>
2022-12-01 08:10:52 -0800

    imported add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,6 @@ if __name__ == "__main__":
     from add_0 import add
 
 """"Print the result of 1+2=3"""
-
-print("{} + {} = {}".format(1, 2, add(1, 2)))
+a=1
+b=2
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
