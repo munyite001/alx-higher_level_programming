3cefed3e169e8e5e041df89beb59008ed41c6f37
munyite001 <emunyite@gmail.com>
2022-12-03 08:53:24 -0800

    Element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,12 +2,17 @@
 
 """Retrieve an element from a given index in an array
 
+    Args:
+        my_list: array
+        idx: index
 
+    Returns:
+        prints the element at given index
 """
 
 
 def element_at(my_list, idx):
-    if idx < 0 || idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return "None"
     else:
         print("Element at index {:d} is {}".format(idx, my_list[idx]))
