abea87977b544d2ea5f4acf625cf9472fb9cf9d7
munyite001 <emunyite@gmail.com>
2022-12-04 09:33:14 -0800

    C-removal

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,9 @@
 
 
 def no_c(my_string):
+    result = ''
     for letter in my_string:
-        if letter == 'C' or letter == 'c':
-            letter = ''
-    return my_string
+        if letter != 'C' and letter != 'c':
+            result += letter
+            
+    return result
