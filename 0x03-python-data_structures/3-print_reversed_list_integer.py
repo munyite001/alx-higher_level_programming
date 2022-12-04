b909d943e4d6529491051568067b09a77c825cb3
munyite001 <emunyite@gmail.com>
2022-12-04 09:19:33 -0800

    reversed order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -11,5 +11,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    for number in reversed(my_list):
-        print("{:d}".format(number))
+    
+    if my_list:
+        for number in reversed(my_list):
+            print("{:d}".format(number))
