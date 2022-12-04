b75a9f4271c4dbaadbeaef116aaa91f2cada4f8e
munyite001 <emunyite@gmail.com>
2022-12-04 08:59:20 -0800

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,20 @@
+#!/sr/bin/python3
+
+"""Replace an element from a given index in an array
+
+    Args:
+        my_list: array
+        idx: index
+        element: the element to be placed at the specified index
+
+    Returns:
+        the modified list
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
