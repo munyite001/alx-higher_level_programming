b27314b92b12f87e007e42d72b7ea81bb1c0f29b
munyite001 <emunyite@gmail.com>
2022-12-08 09:21:33 -0800

    common elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -3,7 +3,7 @@
 
 def common_elements(set_1, set_2):
     unique = []
-    for num in set_1:
-        if num in set_2:
-            unique.append(unique)
+    for elem in set_1:
+        if elem in set_2:
+            unique.append(elem)
     return unique
