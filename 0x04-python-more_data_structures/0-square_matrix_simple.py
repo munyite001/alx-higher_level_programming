e4ffffb762462ce7cac52c3a686dc25a36033f52
munyite001 <emunyite@gmail.com>
2022-12-08 08:46:05 -0800

    square

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return([list(map(lambda x: x*x, row)) for row in matrix])
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for number in row:
+            new_row.append(number**2)
+        new_matrix.append(new_row)
+    return new_matrix
