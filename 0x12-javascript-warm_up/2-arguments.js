9a7cd53e0220a1abcd84fc7634805bcd99aed379
munyite001 <emunyite@gmail.com>
2023-03-13 15:16:22 +0300

    2-arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length < 2)
+{
+    console.log("No Argument");
+}
+else if (process.argv.length == 2)
+{
+    console.log("Argument found");
+}
+else
+{
+    console.log("Arguments found");
+}
