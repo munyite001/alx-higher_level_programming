566dab939271cb5b0b04ff20c114da79e2cabfb7
munyite001 <emunyite@gmail.com>
2023-03-14 20:21:09 +0300

    4-Rectangles

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      process.stdout.write('\n');
+    }
+  }
+
+  rotate () {
+    const x = this.width; const y = this.height;
+    this.width = y; this.height = x;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
