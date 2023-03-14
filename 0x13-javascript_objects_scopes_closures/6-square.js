1075e7a4721a6807e9d31d981bcf8e110d21dcb2
munyite001 <emunyite@gmail.com>
2023-03-14 21:15:25 +0300

    6-square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square/Square') {
   charPrint (c) {
-    const char = c === undefined ? 'X' : 'C';
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    const char = c === undefined ? 'X' : c;
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
         process.stdout.write(char);
       }
       process.stdout.write('\n');
