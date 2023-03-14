fd5702b032f9f2f150dd741420b9d4cfe59b2fa0
munyite001 <emunyite@gmail.com>
2023-03-14 21:26:59 +0300

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
-    charPrint (c) {
-      if (c === undefined) {
-        this.print();
-      } else {
-        for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                process.stdout.write(c);
-            }
-            process.stdout.write('\n');
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          process.stdout.write(c);
         }
+        process.stdout.write('\n');
       }
     }
-  };
-  
+  }
+};
