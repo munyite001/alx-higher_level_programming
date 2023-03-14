db258399f3205af2fdf25d65a98e6d3d714ffaf4
munyite001 <emunyite@gmail.com>
2023-03-14 20:49:09 +0300

    square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.export = class Square extends require('./4-rectangle.js/Rectangle') {
+module.exports = class Square extends require('./4-rectangle.js/Rectangle') {
   constructor (size) {
     super(size, size);
   }
