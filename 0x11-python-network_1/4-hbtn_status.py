893f7e3c4ab1a444ff3eba55a4aeda9027cab0f8
munyite001 <emunyite@gmail.com>
2023-04-02 15:34:43 +0300

    request package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print(f"\t- type: {type(r)}")
-    print(f"\t- content: {r}")
+    print(f"\t- content: {r.content}")
