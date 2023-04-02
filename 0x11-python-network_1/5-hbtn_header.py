52427fcf768df4f21c8f6e492fcd0767fab0439a
munyite001 <emunyite@gmail.com>
2023-04-02 15:52:16 +0300

    request headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Take in a url and an email, send a post request
+"""Take in a url, send a request
 and display contents of X-Request-Id
 """
 import requests
 import sys
 
-
 if __name__ == '__main__':
-    url = sys.argv[1]
-    r = requests.get(url)
-    print(r.headers['X-Request-Id'])
+    r = requests.get(sys.argv[1])
+    print(dict(r.headers).get('X-Request-Id'))
