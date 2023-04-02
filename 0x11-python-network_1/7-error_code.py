68b57013b0844b3cca90be30c15ad120f7c3a3d5
munyite001 <emunyite@gmail.com>
2023-04-02 16:18:05 +0300

    handle error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Send a request to an email
+display body of the response
+handle errors
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    try:
+        r = requests.get(sys.argv[1])
+        print(r.text)
+    except requests.exceptions.RequestException as e:
+        print(f"Error code: {e.code}")
