9705abd9ac86d2ab5ddb209838261f8a0a7c2fa4
munyite001 <emunyite@gmail.com>
2023-04-02 14:25:22 +0300

    Get Headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""take in a url, send a request and display contents of X-Request-Id"""
+"""Take in a url, send a request and display contents of X-Request-Id"""
 import urllib.request
 import sys
 
 
 if __name__ == '__main__':
-    req = urllib.request.Request(f"{sys.argv[1]}")
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
-        print(response.headers['X-Request-Id'])
+        print(dict(response.headers).get('X-Request-Id'))
