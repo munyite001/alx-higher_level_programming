1d7b6036e9caa0f258ca50b238258874fc31be7b
munyite001 <emunyite@gmail.com>
2023-04-02 16:39:19 +0300

    json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""send a post request with a letter
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 2:
+        q=sys.argv[1]
+    else:
+        q=''
+    values = {'q': q}
+    
+    r = requests.post(url, data=values)
+    
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print(f"[{res.get('id')}] {res.get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
