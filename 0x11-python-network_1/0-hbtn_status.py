8416f93486536f1566f46d572abce7e6049f11d9
munyite001 <emunyite@gmail.com>
2023-04-02 13:58:22 +0300

    pycode style issues

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Fetch a url using urllib and display the response
-"""
+"""Fetch a url using urllib and display the response"""
+import urllib.request
+
+
 if __name__ == '__main__':
-    import urllib.request
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
         page = response.read()
         print("Body response:")
-        print(f"\t -type: {type(page)}")
-        print(f"\t -content: {page}")
-        print(f"\t -utf8 content: {page.decode('utf-8')}")
-
+        print(f"\t- type: {type(page)}")
+        print(f"\t- content: {page}")
+        print(f"\t- utf8 content: {page.decode('utf-8')}")
