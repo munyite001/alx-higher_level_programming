d6092b37cb1d4567bee9608d4b171da6a1d8535a
munyite001 <emunyite@gmail.com>
2023-04-25 23:24:43 +0300

    request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,12 +3,9 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
+const path = process.argv[3];
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
-  }
+request(url, (error, body) => {
+  if (error) throw error;
+  fs.writeFile(path, body, 'utf-8');
 });
