e780997551b92ba1b24469c01fa86d4f5b8c53f5
munyite001 <emunyite@gmail.com>
2023-04-25 22:55:14 +0300

    requests

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
+request(url, (error, response) => {
   if (error) throw error;
 
-  console.log(response.statusCode);
+  console.log(`code: ${response.statusCode}`);
 });
