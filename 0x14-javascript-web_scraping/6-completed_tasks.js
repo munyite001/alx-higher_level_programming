753f38936085e28b22a289343d05423afc9d0746
munyite001 <emunyite@gmail.com>
2023-04-25 23:29:23 +0300

    complete

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) throw error;
+  if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
