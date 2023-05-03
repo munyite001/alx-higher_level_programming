89fe2ae6cf1dab17054d7a5b2261db1fe1789146
munyite001 <emunyite@gmail.com>
2023-05-03 22:58:54 +0300

    fetch

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
-    $('div#toggle_header').click(function () {
-      $('header').toggleClass('red');
-      $('header').toggleClass('green');
-      
-    });
+  $('div#toggle_header').click(function () {
+    $('header').toggleClass('red');
+    $('header').toggleClass('green');
   });
-  
+});
